5cb75f058c06433a53b2dd8b9181be7d572e9663
Marcin275143 <73649084+Marcin275143@users.noreply.github.com>
2020-11-22 01:27:59 +0100

    Update transportujFlota.hpp

--- zal/transportujFlota.hpp
+++ zal/transportujFlota.hpp
@@ -4,6 +4,18 @@
 
 unsigned int transportujFlota(unsigned int towar)
 {
-    // Twoja implementacja tutaj
+    int towar_statku = 0;
+    int towar_calkowity = 0;
+    int licznik = 0;
+    for (int towar_sumaryczny = 0; towar_sumaryczny <towar; towar_sumaryczny = towar_sumaryczny + towar_statku)
+    {
+        Stocznia stocznia;
+        Statek* statek = stocznia();
+        towar_statku = statek->transportuj();
+        towar_calkowity = towar_calkowity + towar_statku;
+        Zaglowiec *wskaznik = dynamic_cast<Zaglowiec*>(statek);
+        if (wskaznik != nullptr) licznik++;
+        delete statek;
+    }
     return 0;
 }
